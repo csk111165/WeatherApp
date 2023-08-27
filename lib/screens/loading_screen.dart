142da53bb58976179a308bf32e6328f731ef7eb4
Chandra Shekhar <cshekhar@nvidia.com>
2023-08-27 16:17:08 +0530

    Refactoring the networking logic

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -3,8 +3,8 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:weather_app/services/networking.dart';
+
 
 // const String apiKey = 'b6907d289e10d714a6e88b30761fae22';
 const String apiKey = '4f8619ef2e7f68e984133c8edb59dee4';
@@ -24,54 +24,26 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
 
-    getLocation();
+    getLocationData();
     
   }
 
-  // this function is for extracting the data using api provided by weatehr
-  void getData() async {
-
-     try {
-    http.Response response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'));
-
-    if (response.statusCode == 200) {
-      print("Success");
-      String data = response.body;
-
-      var decodedData = jsonDecode(data);
-
-      double temperature = decodedData['main']['temp'];
-      String weatherDesc = decodedData['weather'][0]['description'];
-      int condition = decodedData['weather'][0]['id'];
-      String cityName = decodedData['name'];
-      print("what is temp in my area $temperature");
-      print("what is cityName $cityName");
-      print("what is weather condition today : $weatherDesc");
-
-    } else {
-      print('status code: ${response.statusCode}');
-      // print("Request failed with status: ${response.body}");
-      print(response.body);
-    }
-  } catch (e) {
-   
-    print("Error: $e");
-  }
   
-  }
-
   // method for getting the current location : Future<void> since we are awaiting to get some result
-  void getLocation() async {
+  void getLocationData() async {
     // create an object of Location class defined in services/location.dart
     Location location = Location();
     await location.getCurrentLocation();
     latitude = location.latitude;
     longitude = location.longitude;
 
+    NetworkHelper networkHelper = NetworkHelper(url: 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
+
     print("what is lat : ${location.latitude}");
     print("what is longi  ${location.longitude}");
 
-    getData();
   }
 
   @override
---
+++ lib/services/networking.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+  final String url;
+
+  // )
+
+  NetworkHelper({required this.url});
+
+  // return Future since the result of get is not available on the fly
+  Future getData() async {
+    http.Response response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      print("Success");
+      String data = response.body;
+      return jsonDecode(data);
+     
+
+    } else {
+      print('status code: ${response.statusCode}');
+      // print("Request failed with status: ${response.body}");
+      print(response.body);
+    }
+  }
+}

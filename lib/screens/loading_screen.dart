35218fc74350626b48ea6cbe3d1a3deb070347fa
Chandra Shekhar <cshekhar@nvidia.com>
2023-08-27 15:39:54 +0530

    Updating the api url

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/services/location.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+// const String apiKey = 'b6907d289e10d714a6e88b30761fae22';
+const String apiKey = '4f8619ef2e7f68e984133c8edb59dee4';
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,19 +16,23 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  double? latitude;
+  double? longitude;
+
   // this method is called each time the app is created at very first time
   @override
   void initState() {
+
     getLocation();
-    getData();
+    
   }
 
-  
   // this function is for extracting the data using api provided by weatehr
   void getData() async {
 
      try {
-    http.Response response = await http.get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
+    http.Response response = await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'));
 
     if (response.statusCode == 200) {
       print("Success");
@@ -34,24 +44,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
       String weatherDesc = decodedData['weather'][0]['description'];
       int condition = decodedData['weather'][0]['id'];
       String cityName = decodedData['name'];
-
-
+      print("what is temp in my area $temperature");
+      print("what is cityName $cityName");
+      print("what is weather condition today : $weatherDesc");
 
     } else {
       print('status code: ${response.statusCode}');
       // print("Request failed with status: ${response.body}");
+      print(response.body);
     }
   } catch (e) {
    
     print("Error: $e");
   }
-  //   if (response.statusCode == 200) {
-  //   // Process the response
-  //   print("successfully loaded the page");
-  //   // print("Response data: ${response.body}");
-  // } else {
-  //   print("Request failed with status: ${response.statusCode}");
-  // }
+  
   }
 
   // method for getting the current location : Future<void> since we are awaiting to get some result
@@ -59,8 +65,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
     // create an object of Location class defined in services/location.dart
     Location location = Location();
     await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+
     print("what is lat : ${location.latitude}");
     print("what is longi  ${location.longitude}");
+
+    getData();
   }
 
   @override

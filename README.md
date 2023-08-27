0393b743b321f8936daef748bed9ad2517093b2a
Chandra Shekhar <cshekhar@nvidia.com>
2023-08-27 11:21:24 +0530

    Updating the readme for issue in http

--- README.md
+++ README.md
@@ -10,7 +10,13 @@ A few resources to get you started if this is your first Flutter project:
 
 - [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
 - [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+
+
+## Notes and links
+* If we use http module, we won't be able to able to use `get` or `post` directly we need to perform this step otherwise it will
+  throw client error : 
+  * Go to flutter\bin\cache and remove flutter_tools.stamp file
+
+  * Go to flutter\packages\flutter_tools\lib\src\web open file chrome.dart and edit it Add '--disable-web-security' and comment '--disable-extensions' line. 
+  * See the stackoverflow link [link](https://stackoverflow.com/questions/60191683/xmlhttprequest-error-in-flutter-web-enabling-cors-aws-api-gateway)
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,22 +9,45 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   // this method is called each time the app is created at very first time
   @override
   void initState() {
-     getLocation();
+    getLocation();
+    getData();
+  }
+
+  
+  // this function is for extracting the data using api provided by weatehr
+  void getData() async {
+
+     try {
+    http.Response response = await http.get(Uri.parse('https://www.google.com/'));
+
+    if (response.statusCode == 200) {
+      print("Success");
+      print("body : ${response.body}");
+    } else {
+      print("Request failed with status: ${response.body}");
+    }
+  } catch (e) {
+    print("Error: $e");
+  }
+  //   if (response.statusCode == 200) {
+  //   // Process the response
+  //   print("successfully loaded the page");
+  //   // print("Response data: ${response.body}");
+  // } else {
+  //   print("Request failed with status: ${response.statusCode}");
+  // }
   }
 
   // method for getting the current location : Future<void> since we are awaiting to get some result
- void  getLocation() async {
-   
-   // create an object of Location class defined in services/location.dart
-   Location location = Location();
-   await location.getCurrentLocation();
-   print("what is lat : ${location.latitude}");
-   print("what is longi  ${location.longitude}");
-    
+  void getLocation() async {
+    // create an object of Location class defined in services/location.dart
+    Location location = Location();
+    await location.getCurrentLocation();
+    print("what is lat : ${location.latitude}");
+    print("what is longi  ${location.longitude}");
   }
 
   @override

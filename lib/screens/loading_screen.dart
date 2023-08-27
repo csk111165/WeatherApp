a0b60e8a3d750c6f912752716a3aa6a0f08dd8fd
Chandra Shekhar <cshekhar@nvidia.com>
2023-08-27 17:31:54 +0530

    Adding route to LocationScreen from loading screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,14 +1,14 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/screens/location_screen.dart';
 import 'package:weather_app/services/location.dart';
 import 'package:weather_app/services/networking.dart';
 
-
 // const String apiKey = 'b6907d289e10d714a6e88b30761fae22';
 const String apiKey = '4f8619ef2e7f68e984133c8edb59dee4';
-
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -16,22 +16,19 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   double? latitude;
   double? longitude;
 
   // this method is called each time the app is created at very first time
   @override
   void initState() {
-
+    print("initSTate is called");
     getLocationData();
-    
   }
 
-  
   // method for getting the current location : Future<void> since we are awaiting to get some result
   void getLocationData() async {
-
+    print("getLocationData is called");
     // create an object of Location class defined in services/location.dart
     // it will give the locaiton which contains information about lat and lon
     Location location = Location();
@@ -41,22 +38,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
     //----------------------------------------------------------------------
 
     // here the NetworkHelper will use the lat and lon value to fetch the information from api using getData
-    NetworkHelper networkHelper = NetworkHelper(url: 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    NetworkHelper networkHelper = NetworkHelper(
+        url:
+            'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
     var weatherData = await networkHelper.getData();
 
-
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //getLocation();
-          },
-          child: Text('Get Location'),
+        child: SpinKitDoubleBounce(
+          color: Colors.red,
+          size: 100,
         ),
       ),
     );
